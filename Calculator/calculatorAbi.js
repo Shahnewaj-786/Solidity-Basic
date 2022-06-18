92fdb48090572ae95b8f8f0f1ca613d86f58531a
Md Shahnewaj Sheikh <shahanewajshaikh786@gmail.com>
2022-06-18 19:47:05 +0600

    Area calculator using solidity

---
+++ Calculator/calculatorAbi.js
@@ -0,0 +1,33 @@
+calculatorAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "h",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "w",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateArea",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getArea",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "area",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
